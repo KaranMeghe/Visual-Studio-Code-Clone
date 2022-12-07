62440ab95efbbbe8f972c4312c45cf8a6fa0df30
Karan Meghe <karanmeghe87@gmail.com>
2022-12-08 02:13:10 +0530

    Toggle update V1

--- script.js
+++ script.js
@@ -2,26 +2,17 @@ const button = document.getElementById('downloads');
 const accordion = document.getElementById('accordion');
 const icon = document.getElementById('icon');
 const result = button.contains(icon);
-const btn = document.querySelector('#menu-btn');
+const btn = document.getElementById('menu-btn');
 const menu = document.querySelector('#menu');
-
-// navToggle()
-function navToggle(){
-    btn.classList.toggle('open');
-    menu.classList.toggle('flex');
-    menu.classList.toggle('hidden');
-    
-    if(menu.classList.contains('flex')){
-        logo.setAttribute('src','./images/logo-bookmark-footer.svg')
-    } else {
-        logo.setAttribute('src','./images/logo-bookmark.svg')   
-    }
-}
+const background = document.getElementById('background');
 
 // Hamburger event listner
-btn.addEventListener('click', navToggle);
-
-
+btn.addEventListener('click', ()=>{
+background.classList.add('background');
+// btn.classList.toggle('open');
+menu.classList.toggle('flex');
+menu.classList.toggle('hidden');
+});
 
 // Event listner on download button
 button.addEventListener('click', ()=>{
