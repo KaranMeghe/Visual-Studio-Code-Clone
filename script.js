92f97982be10b4f031044ad1919d30ae7884ed2c
Karan Meghe <karanmeghe87@gmail.com>
2022-12-07 14:19:50 +0530

    Event  updates for Hero BTN

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const button = document.getElementById('downloads');
 const accordion = document.getElementById('accordion');
 
+// Event listner for button
 button.addEventListener('click', ()=>{
     accordion.classList.toggle('active');
+})
+
+// Event listner for all document
+window.addEventListener('click', (e)=>{
+ if(e.target !== button){
+    accordion.classList.remove('active');
+ }
 })
