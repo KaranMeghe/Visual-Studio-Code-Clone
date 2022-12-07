f1cfdac76d9d36a674d70eeb80ef93d390bb11c2
Karan Meghe <karanmeghe87@gmail.com>
2022-12-07 20:25:56 +0530

    Hamburger and mobile menu v0.1

--- script.js
+++ script.js
@@ -2,6 +2,25 @@ const button = document.getElementById('downloads');
 const accordion = document.getElementById('accordion');
 const icon = document.getElementById('icon');
 const result = button.contains(icon);
+const btn = document.querySelector('#menu-btn');
+const menu = document.querySelector('#menu');
+
+// navToggle()
+function navToggle(){
+    btn.classList.toggle('open');
+    menu.classList.toggle('flex');
+    menu.classList.toggle('hidden');
+    
+    if(menu.classList.contains('flex')){
+        logo.setAttribute('src','./images/logo-bookmark-footer.svg')
+    } else {
+        logo.setAttribute('src','./images/logo-bookmark.svg')   
+    }
+}
+
+// Hamburger event listner
+btn.addEventListener('click', navToggle);
+
 
 
 // Event listner on download button
@@ -13,5 +32,5 @@ button.addEventListener('click', ()=>{
 window.addEventListener('click', (e)=>{
     if(e.target !== button){
         accordion.classList.remove('active');
-     }
+     } 
    });
